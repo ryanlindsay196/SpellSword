c948048d6984fcd39a4f37e89d3166c1e0af76f6
rlindsa@masonlive.gmu.edu <rlindsa@masonlive.gmu.edu>
2020-04-28 13:36:16 -0400

    Sped up swap to book animation

---
+++ Spellsword/Assets/Scripts/Objects/BloodSplatter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplatter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SelfDelete", 0.5f);
+    }
+
+    void SelfDelete()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

bb6376f0bc067d5bd1e4b7d0527ff296b39a1ac2
Connor McNamara <cmcnama6@masonlive.gmu.edu>
2020-05-04 14:39:40 -0400

    Beginning to connect boss phases together and refine how it works. Will continue testing and polishing, i hope this turns out well

---
+++ Spellsword/Assets/BossSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    public GameObject MeleeMan;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private int numEnemies = 0;
+    public int spawnDelay;
+
+    private float timeSinceLastSpawn;
+    public float maxEnemies;
+    public GameObject portal;
+
+    public GameObject Spawner;
+    void Start()
+    {
+        //Spawner = GameObject.FindGameObjectWithTag("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numEnemies = enemies.Count;
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (numEnemies < maxEnemies && timeSinceLastSpawn >= spawnDelay)
+        {
+            GameObject spawnedGhoul = Instantiate(MeleeMan, Spawner.transform.position, Quaternion.identity);
+            enemies.Add(spawnedGhoul);
+            timeSinceLastSpawn = 0;
+        }
+
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy == null)
+            {
+                enemies.Remove(enemy);
+                maxEnemies--;
+            }
+        }
+        if(maxEnemies == 0)
+        {
+            portal.SetActive(false);
+        }
+    }
+}

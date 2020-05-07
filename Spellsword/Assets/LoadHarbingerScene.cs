d5757daba4ded77ce56f4457952db63d8e8ab6ce
Zach G <zachariah0123@gmail.com>
2020-05-07 18:02:51 -0400

    Added weaver fight room. Added green crystals to 3rd portal, corrosion rooms and weaver 3 fight. new portal that spawns after defeating weaver 3 that loads harbinger fight. Removed both enchantments. Deleted room at bot of pit.

---
+++ Spellsword/Assets/LoadHarbingerScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadHarbingerScene : MonoBehaviour
+{
+    public int harbingerScene;
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<CharacterMovement>() != null)
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            SceneManager.LoadScene(harbingerScene);
+        }
+    }
+}

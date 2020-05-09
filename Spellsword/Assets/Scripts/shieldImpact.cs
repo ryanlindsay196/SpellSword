b30e445dbb682e7ef9912b3e31efa8ba3a0a640b
jmcgreev <jmcgreev@gmu.edu>
2020-05-08 20:31:00 -0400

    trying to make new sounds

---
+++ Spellsword/Assets/Scripts/shieldImpact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shieldImpact : MonoBehaviour
+{
+
+    public AudioClip shieldImpactClip;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "sword")
+        {
+            gameObject.GetComponent<AudioSource>().clip = shieldImpactClip;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
